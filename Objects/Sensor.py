0f47e699bcc8684df41eb362e2e25d9398bb9d6a
Santiago Jaimes <36584120+Santiago1325@users.noreply.github.com>
2023-03-10 17:09:23 -0500

    Modified sensor

--- Objects/Sensor.py
+++ Objects/Sensor.py
@@ -8,7 +8,10 @@ class Sensor:
 # with top = 0 and left = 0 being the upper left corner of the screen.
 # Leave width or height as full to use the full widht or height of the screen
 # If only the monitor is provided it will take a full screenshot
-    def __init__(self, monitor_num, top = 0, left = 0, width = "full", height = "full"):
+#(left, top, width, height)
+
+
+    def __init__(self, monitor_num, box = None):
         
         #Screenshot taker
         self.sct = mss()
@@ -20,27 +23,29 @@ class Sensor:
         self.monitor = self.sct.monitors[monitor_num]
         print(self.monitor)
         
-        #Checking default arguments
-        if width == "full":
-            width = self.monitor["width"]
-        if height == "full":
-            height = self.monitor["height"]
-
-        #Verifying that nor width or height exceed the monitor dimensions
-        if width + left > self.monitor["width"] or height + top > self.monitor["height"] or left >= self.monitor["width"] or top >= self.monitor["height"]:
-            raise Exception("Arguments exceed the dimensions of the monitor")
-        
-        self.bbox = {"top": self.monitor["top"] + top, "left": self.monitor["left"] + left, "width": width, "height": height}
-        print(self.bbox)
+        if not box:
+            self.box = self.monitor
+        else:
+            if type(box) != tuple:
+                raise Exception("Box must be a tuple")
+            elif len(box) > 4 or len(box) < 4:
+                raise Exception("Tuple len must have 4 elements")    
+            else:
+                if box[1] >= self.monitor["height"] or box[0] >= self.monitor["width"] or box[0] + box[2] > self.monitor["width"] or  box[1] + box[3] > self.monitor["height"]:
+                    raise Exception("One or more of the given dimensions exceed the monitor dimensions")
+                if box[0] > box[2] or box[1] > box[3]:
+                    raise Exception("Dimensions don't match")
+                self.box = {"left":box[0], "top":box[1], "width":box[2], "height":box[3]}
+            
         
     #Take screenshot
     def grab_img(self):
-        img = self.sct.grab(self.bbox)
+        img = self.sct.grab(self.box)
         return np.array(img)
         
 
 #Example
-s = Sensor(2,0,0,500)
+s = Sensor(1, (0,0,1920,500))
 
 while True:
     img = s.grab_img()
