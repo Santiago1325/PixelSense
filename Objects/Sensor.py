0957e3e52092d09cda5d82226eee29de037c6c13
Santiago Jaimes <36584120+Santiago1325@users.noreply.github.com>
2023-03-24 20:41:22 -0500

    Implement Mask Sensor

--- Objects/Sensor.py
+++ Objects/Sensor.py
@@ -2,6 +2,27 @@ import numpy as np
 import cv2
 from mss import mss
 from abc import ABC, abstractmethod
+import time
+
+
+import magichue
+
+
+TOKEN = 'eyJhbGciOiJSUzI1NiJ9.eyJhcHBLZXkiOiJaRzAwMSIsImNkcElkIjoiWkcwMDEiLCJjbGllbnRJZCI6Ik1ON1dBMVZZSjdBUFNHTTRNWDg3SENaNkQyMU5EWFAyIiwiZXhwaXJlRGF0ZSI6MTY5Mzc5MDkxMTY3OSwiZ3Vlc3QiOmZhbHNlLCJsb2dpbkRhdGUiOjE2NzgyMzg5MTE2NzksInJlZnJlc2hEYXRlIjoxNjg4NjA2OTExNjc5LCJzZXJ2ZXJDb2RlIjoiVVMiLCJ1aWQiOjE2MzMyMzU2ODAzOTQzMzgzMDYsInVzZXJJZCI6InNhbnRpYWdvajIyMkBnbWFpbC5jb20ifQ.n0GkAldq_xBzOgfnUskmBKWnAlSoujuzJs54WVP8KDpQ6AsJIBnvoqnxohbf-UBTrlwqlNiINT92irboO9e-fKc53dRqz-JQW3_C2FMls_lF5V10qE05fg91wfTKfvCJGdYTyqAKgV6SjCYucpb4O5DflbAlqhrAe49BE2HEw7nbGRnJsx9YMoO0ZZ6RpulF6CLqthz7qBl-0DgtQfTHecD3RJ7COvBl_qigFOvI8EHt83Mzjb77LT7dAlNW27oqC0115HFsW4duUujK4PxJPQhg9jKPY40DzTaiqYakHCZ7CkrJxGlGPScDGSk6Do5A_9CvcaYW7jRZT5LvZD2Caw'
+api = magichue.RemoteAPI.login_with_token(TOKEN)
+light = api.get_online_bulbs()[0]
+light.rgb = (0,255,0)
+
+PoliceLights = magichue.CustomMode(
+    mode = magichue.MODE_JUMP,
+    speed = 0.85,
+    colors= [(255, 0, 0),   (0, 0, 255)],
+)
+
+police_pursuit = False
+
+
+
 
 #ToDo
 #MaskSensor
@@ -24,7 +45,7 @@ class Sensor(ABC):
         
 
         
-class ImageSensor(Sensor):
+class MaskSensor(Sensor):
     """
     Base class of the Sensor. Will recieve the monitor and the area that will screenshot
     with top = 0 and left = 0 being the upper left corner of the screen.
@@ -34,10 +55,16 @@ class ImageSensor(Sensor):
     """   
     
     
-    def __init__(self, monitor_num, box=None):
+    def __init__(self, monitor_num, mask = None, treshold = None, background = np.array([0,0,0]),box=None):
         
         #Screenshot taker
         self.sct = mss()
+        
+        self.mask = mask.astype(float)
+        
+        self.treshold = treshold
+        
+        self.background = background
         
         #Verifying the number of monitors
         if monitor_num > len(self.sct.monitors) - 1:
@@ -61,17 +88,45 @@ class ImageSensor(Sensor):
             
     def grab_data(self):
         img = self.sct.grab(self.box)
-        return np.array(img)
+        return np.array(img)[...,:3].astype(float)
+    
+    def trigger(self):
+        pw_abs = abs(self.grab_data() - self.mask).astype(np.uint8)
+        bool_mask =  (self.mask == self.background).all(axis=2,  keepdims=True).astype(int)
+        bool_mask = np.ones(bool_mask.shape) - bool_mask
+        mean_sum = np.mean(bool_mask * pw_abs)
+        print(mean_sum)
+        return mean_sum < self.treshold
+    
+    
     
 #Example
-s = ImageSensor(1, (720,100,1000,500))
+#(left, top, widht, height)
+gta_mask = cv2.imread("C:/Users/Santiago/Pictures/gta_stars_mask.png")
+s = MaskSensor(1, mask = gta_mask, treshold = 1, box = (1861,9,34,47))
+
+time_check = 3
 
 while True:
-    img = s.grab_img()
-    cv2.imshow('screen', img)
- 
+    #img = s.grab_data().astype(np.uint8)
+    #cv2.imshow('screen', img)
+    #cv2.waitKey(1000)
+    #cv2.imshow('screen', gta_mask)
+    #cv2.waitKey(1000)
+    police_pursuit = s.trigger()
+    if police_pursuit and light.rgb == (0,255,0):
+        light.mode = magichue.RED_BLUE_CROSSFADE
+        time.sleep(5)
+    elif not police_pursuit and light.mode == magichue.RED_BLUE_CROSSFADE:
+        light.rgb = (0,255,0)
+        time.sleep(1)
+    
+    
+        
+    #cv2.waitKey(2000)
 
-    if (cv2.waitKey(1) & 0xFF) == ord('q'):
-        cv2.destroyAllWindows()
-        break
+
+    #if (cv2.waitKey(1) & 0xFF) == ord('q'):
+        #cv2.destroyAllWindows()
+        #break
     
